a9f82db5cb26e842c56f4b2b07c40beee066465c
Higor Prado <higorevop@gmail.com>
2026-08-01 21:02:10 -0300

    Scale stress workers to CPU count

--- stress_helper.py
+++ stress_helper.py
@@ -1,12 +1,20 @@
+import os
 import subprocess
 import atexit
 
 _process = None
 
+def _cpu_workers():
+    try:
+        n = len(os.sched_getaffinity(0))
+    except (AttributeError, OSError):
+        n = os.cpu_count()
+    return n if n and n > 0 else 1
+
 def stress_start():
     global _process
     if _process is None:
-        _process = subprocess.Popen(["stress", "--cpu", "12"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        _process = subprocess.Popen(["stress", "--cpu", str(_cpu_workers())], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 def stress_stop():
     global _process
